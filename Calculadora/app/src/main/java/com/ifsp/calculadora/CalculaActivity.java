5d0f95c89b288997d92fbbb5364b5909e15ca5c1
Moises <olimpiodev.tec@gmail.com>
2025-02-11 22:05:55 -0300

    Navegação entre telas com passagem de parâmetros

---
+++ Calculadora/app/src/main/java/com/ifsp/calculadora/CalculaActivity.java
@@ -0,0 +1,45 @@
+package com.ifsp.calculadora;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CalculaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_calcula);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setarTitulo();
+    }
+
+    public void setarTitulo() {
+        Intent telaHome = getIntent();
+        String titulo = telaHome.getStringExtra("parametro");
+
+        TextView tvTitulo = findViewById(R.id.tvTitulo);
+        tvTitulo.setText(titulo);
+    }
+
+
+}
+
+
+
+
+
+
+
--- Calculadora/app/src/main/java/com/ifsp/calculadora/HomeActivity.java
+++ Calculadora/app/src/main/java/com/ifsp/calculadora/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.ifsp.calculadora;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,42 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnAdicao = findViewById(R.id.btnAdicao);
+        btnAdicao.setOnClickListener(this);
+
+        Button btnSubtracao = findViewById(R.id.btnSubtracao);
+        btnSubtracao.setOnClickListener(this);
+
+        Button btnMultiplicacao = findViewById(R.id.btnMultiplicacao);
+        btnMultiplicacao.setOnClickListener(this);
+
+        Button btnDivisao = findViewById(R.id.btnDivisao);
+        btnDivisao.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent telaCalcula = new Intent(this, CalculaActivity.class);
+
+        if (v.getId() == R.id.btnAdicao) {
+            telaCalcula.putExtra("parametro", "Somar Números");
+        } else if (v.getId() == R.id.btnSubtracao) {
+            telaCalcula.putExtra("parametro", "Subtrair Números");
+        } else if (v.getId() == R.id.btnMultiplicacao) {
+            telaCalcula.putExtra("parametro", "Multiplicar Números");
+        } else if (v.getId() == R.id.btnDivisao) {
+            telaCalcula.putExtra("parametro", "Dividir Números");
+        }
+        startActivity(telaCalcula);
     }
 }
+
+
+
+
+
+
+
+
+
